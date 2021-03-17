3ed9d5ca4953ecafa8a6cc4410f53d6b744b8a6d
badzohugues <hugues.badzoukoula@gmail.com>
2021-03-17 14:24:40 +0100

    Initial commit

---
+++ app/src/main/java/com/badzohugues/staticlbcapp/ui/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.badzohugues.staticlbcapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.badzohugues.staticlbcapp.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
